05eea3a71b1b1c94f918be728bb9a1a42daa7ddb
Harrison Groemer <160081765+harryg1244@users.noreply.github.com>
2024-11-19 23:34:14 +1000

    convert_m_to_km.py

--- prac_08/convert_m_to_km.py
+++ prac_08/convert_m_to_km.py
@@ -1,14 +1,5 @@
-"""
-CP1404 Week 11 Workshop - GUI program to convert miles to kilometres
-Lindsay Ward, IT@JCU
-06/10/2015
-"""
-
 from kivy.app import App
 from kivy.lang import Builder
-
-__author__ = 'Lindsay Ward'
-
 from kivy.properties import StringProperty
 
 MILES_TO_KM = 1.60934
